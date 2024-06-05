ac45b3cf958fed7d7ab3a3c24124e9d71395ef3c
Stefano Gemelli <stefanogemelli06@gmail.com>
2024-06-05 20:50:21 -0300

    feat: add shadcn button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <p className="text-3xl font-bold text-indigo-500">Hello Discord Clone</p>;
+  return (
+    <div className="">
+      <p className="text-3xl font-bold text-indigo-500">Hello Discord Clone</p>
+      <Button variant="ghost">Click me</Button>
+    </div>
+  );
 }
